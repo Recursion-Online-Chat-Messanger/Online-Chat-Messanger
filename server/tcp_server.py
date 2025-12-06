9cb3883567b87092969aa20560868136a15ca0c1
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-12-06 18:13:16 +0900

    refactor: リスニング開始メッセージの削除

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -34,13 +34,9 @@ class TCPServer:
     
     # サーバ起動   
     def start(self):
-        print(">>> TCPServer.start() CALLED")
-
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.host, self.port))
         self.sock.listen(20)
-
-        print(f"[TCP SERVER] Listening on {(self.host, self.port)}")
 
         while True:
             conn, addr = self.sock.accept()
